da7038d20c3166fdff4b60f1e8a38884218362df
shuu-glitch <90470133+shuu-glitch@users.noreply.github.com>
2022-03-27 17:04:15 -0400

    Update event.js

--- docs/website/assets/js/event.js
+++ docs/website/assets/js/event.js
@@ -20,15 +20,15 @@ AFRAME.registerComponent('markerhandler', {
         
         this.el.sceneEl.addEventListener('markerFound', () => {
             if (animatedMarker1 && aEntity1) {
-                if (confirm("Open this link?")) {
+                if (confirm("Open this link?") == true) {
                     // redirect to custom URL.
                     window.open('https://en.wikipedia.org/wiki/Arthropleura', '_blank');
                 } else {
                     // disable the event that would be triggered when the marker is on screen
                 }
-                
+
             } else if (animatedMarker2 && aEntity2) {
-                if (confirm("Open this link?")) {
+                if (confirm("Open this link?") == true) {
                     // redirect to custom URL.
                     window.open('https://en.wikipedia.org/wiki/Pulmonoscorpius', '_blank');
                 } else {
@@ -36,7 +36,7 @@ AFRAME.registerComponent('markerhandler', {
                 }
 
             } else if (animatedMarker3 && aEntity3) {
-                if (confirm("Open this link?")) {
+                if (confirm("Open this link?") == true) {
                     // redirect to custom URL.
                     window.open('https://en.wikipedia.org/wiki/Meganeura', '_blank');
                 } else {
@@ -44,14 +44,14 @@ AFRAME.registerComponent('markerhandler', {
                 }
 
             } else if (animatedMarker4 && aEntity4) {
-                if (confirm("Open this link?")) {
+                if (confirm("Open this link?") == true) {
                     // redirect to custom URL.
                     window.open('https://dinopedia.fandom.com/wiki/Arthrolycosa', '_blank');
                 } else {
                     // disable the event that would be triggered when the marker is on screen
                 }
 
-            } else if (animatedMarker5 && aEntity5) {
+            } else if (animatedMarker5 && aEntity5 == true) {
                 if (confirm("Open this link?")) {
                     // redirect to custom URL.
                     window.open('https://en.wikipedia.org/wiki/Redback_spider', '_blank');
@@ -59,7 +59,7 @@ AFRAME.registerComponent('markerhandler', {
                     // disable the event that would be triggered when the marker is on screen
                 }
 
-            } else {
+            } else if (animatedMarker6 && aEntity6 == true){
                 if (confirm("Open this link?")) {
                     // redirect to custom URL.
                     window.open('https://en.wikipedia.org/wiki/Proterogyrinus', '_blank');
